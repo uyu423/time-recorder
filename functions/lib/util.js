ecd02c502a02c584e6a1d69e718f2a91c0ea83a6
Yu Yongwoo <me@yowu.dev>
2020-04-27 15:41:57 +0900

    Revert "newrelic apm 추가"
    This reverts commit 95f161eb08c62ac68170bcac2a2ea1f7675916fc.

--- functions/lib/util.js
+++ functions/lib/util.js
@@ -1,13 +1,6 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const luxon = __importStar(require("luxon"));
+const luxon = require("luxon");
 class Util {
     static hiMsg() {
         return `👋 어서오세요. ${this.dateTimeShort()}`;
@@ -17,64 +10,21 @@ class Util {
     }
     static dateTimeShort() {
         const time = luxon.DateTime.local();
-        return time
-            .setLocale('ko-kr')
-            .setZone('Asia/Seoul')
-            .toLocaleString(luxon.DateTime.DATETIME_SHORT);
-    }
-    static toDateTimeShort(timeStr) {
-        const time = luxon.DateTime.fromISO(timeStr);
-        return time
-            .setLocale('ko-kr')
-            .setZone('Asia/Seoul')
-            .toLocaleString(luxon.DateTime.DATETIME_SHORT);
+        return time.setLocale('ko-kr').setZone('Asia/Seoul').toLocaleString(luxon.DateTime.DATETIME_SHORT);
     }
     static currentTimeStamp() {
         const time = luxon.DateTime.utc();
         return time.toISO();
     }
-    static formatStrToDateTime(date, format) {
-        const time = luxon.DateTime.fromFormat(date, format).toUTC();
-        return time;
-    }
     static currentDate() {
-        return this.currentDateWithFormat('yyyyLLdd');
-    }
-    static currentDateWithFormat(format) {
         const time = luxon.DateTime.local();
-        return time
-            .setLocale('ko-kr')
-            .setZone('Asia/Seoul')
-            .toFormat(format);
+        return time.setLocale('ko-kr').setZone('Asia/Seoul').toFormat('yyyyLLdd');
     }
     static getBetweenDuration(a, b) {
         const aTime = luxon.DateTime.fromISO(a);
         const bTime = luxon.DateTime.fromISO(b);
         const duration = bTime.diff(aTime).normalize();
         return duration;
-    }
-    static isEmpty(value) {
-        if (value === undefined || value === null) {
-            return true;
-        }
-        if (typeof value === 'number' && isNaN(value)) {
-            return true;
-        }
-        if (typeof value === 'string' && value === '') {
-            return true;
-        }
-        if (typeof value === 'object' && Array.isArray(value) && value.length < 1) {
-            return true;
-        }
-        if (typeof value === 'object' &&
-            !(value instanceof Date) &&
-            Object.keys(value).length < 1) {
-            return true;
-        }
-        return false;
-    }
-    static isNotEmpty(value) {
-        return !Util.isEmpty(value);
     }
 }
 exports.Util = Util;
--- functions/src/server.ts
+++ functions/src/server.ts
@@ -6,11 +6,6 @@ import app from './app';
 
 const log = debug('tr:server');
 
-if (process.env.RUN_ENV === 'server') {
-  require('newrelic');
-  console.log('NODE_ENV is production, so execute Newrelic agent.');
-}
-
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
